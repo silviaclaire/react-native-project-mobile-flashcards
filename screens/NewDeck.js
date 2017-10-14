1c5ef8f2a1ed3eb2f022212c90e4cba5b14ab0d7
Claire Yan <silviaclaire@gmail.com>
2017-10-14 20:44:16 +0900

    Build NewDeck screen

--- screens/NewDeck.js
+++ screens/NewDeck.js
@@ -1,13 +1,41 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Keyboard } from 'react-native'
 import { white, black } from '../utils/colors'
+import Button from '../components/Button'
+import * as API from '../utils/api'
 
 export default class NewDeck extends React.Component {
+  state = {
+    title: ''
+  }
+
+  submit = () => {
+    const { title } = this.state
+    API.saveDeckTitle(title)
+
+    Keyboard.dismiss()
+
+    this.props.navigation.navigate(
+      'IndividualDeck',
+      { deckId: title }
+    )
+  }
   render() {
+    const { title } = this.state
+
     return (
-      <View style={styles.container}>
-        <Text>New Deck View</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior='padding'>
+        <Text style={styles.header}>What is the title of your new deck?</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(title) => this.setState({ title })}
+          value={title}
+          placeholder={'Deck Title'}
+        />
+        <Button onPress={this.submit}>
+          Submit
+        </Button>
+      </KeyboardAvoidingView>
     )
   }
 }
@@ -18,5 +46,18 @@ const styles = StyleSheet.create({
     backgroundColor: white,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30,
   },
+  header: {
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  textInput: {
+    height: 40,
+    width: 300,
+    marginVertical: 20,
+    borderColor: black,
+    borderWidth: 1,
+    borderRadius: 5,
+  }
 })
