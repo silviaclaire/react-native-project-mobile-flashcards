cc6252a5535549b7f38adafdd440630331f8a926
Claire Yan <silviaclaire@gmail.com>
2017-10-14 20:43:32 +0900

    Build NewQuestion screen

--- screens/NewQuestion.js
+++ screens/NewQuestion.js
@@ -1,13 +1,52 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView } from 'react-native'
 import { white, black } from '../utils/colors'
+import Button from '../components/Button'
+import * as API from '../utils/api'
+import { guid } from '../utils/helpers'
 
 export default class NewQuestion extends React.Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  submit = () => {
+    const title = this.props.navigation.state.params.title
+
+    API.addCardToDeck(title, {
+      id: guid(),
+      question: this.state.question,
+      answer: this.state.answer
+    })
+
+    this.props.navigation.navigate(
+      'IndividualDeck',
+      { deckId: title }
+    )
+  }
+
   render() {
+    const { question, answer } = this.state
+
     return (
-      <View style={styles.container}>
-        <Text>New Question View</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior='padding'>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(question) => this.setState({ question })}
+          value={question}
+          placeholder={'Question'}
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(answer) => this.setState({ answer })}
+          value={answer}
+          placeholder={'Answer'}
+        />
+        <Button onPress={this.submit}>
+          Submit
+        </Button>
+      </KeyboardAvoidingView>
     )
   }
 }
@@ -19,4 +58,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    height: 40,
+    width: 300,
+    marginVertical: 20,
+    borderColor: black,
+    borderWidth: 1,
+    borderRadius: 5,
+  }
 })
