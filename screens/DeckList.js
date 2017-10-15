c6f9045eed79298f4b4219bcc8728704d3542863
Claire Yan <silviaclaire@gmail.com>
2017-10-15 15:07:13 +0900

    Fix: DeckList not showing last element

--- screens/DeckList.js
+++ screens/DeckList.js
@@ -54,6 +54,7 @@ export default class DeckList extends React.Component {
             </Text>
           </TouchableOpacity>
         ))}
+        <View style={{ height: 50 }}></View>
       </ScrollView>
     )
   }
